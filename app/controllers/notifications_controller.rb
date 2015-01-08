4304be3d3a7c85cfd010c22d24ff058ce0d9ac36
Alison Tague <alisonmtague@gmail.com>
2015-01-07 20:32:26 -0500

    done

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,11 +1,15 @@
 class NotificationsController < ApplicationController
 
+
   def create
     #add the code that will create a new notification and text it to the provided number 
+    @notification = Notification.new
+    @notification.text
+    render :index
   end
 
   def index
-  	
+  	@notification = Notification.new
   end
 
   private
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,17 +5,23 @@ class Notification
 
   def initialize(options={})
     self.to_number = options[:to_number]
-    account_sid = #ENV["twilio_sid"]
-    auth_token =  #ENV["twilio_token"]
+    account_sid = ENV["twilio_sid"]
+    auth_token =  ENV["twilio_token"]
     @client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 
   def text
     # Sends a text to your phone number
+    @client.messages.create(
+      from: '+19549476202',
+      to: '+13058151006',
+      body: random_compliment
+    )
   end
 
+
   def random_compliment
-    compliments = [] #enter compliments there
-    # Generates a random compliment to send to the textee
+    compliments = ["Youre fun!", "You are responsible.", "Happy Birthday!"]
+    compliments.sample
   end
 end
